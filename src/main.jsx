9af9047310522001df6c8ebd138091719db2101c
Bhavesh Pawar <pawarbhavesh16@gmail.com>
2024-08-04 17:50:00 +0530

    import and export the components from one file

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import User from './components/User/User.jsx'
-import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import {Home, About, Contact, User, Github, githubInfoLoader } from './components/index.js'
 
 // const router = createBrowserRouter([
 //   {
+
 //     path: '/',
 //     element: <Layout/>,
 //     children: [
@@ -38,11 +34,7 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
-      <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github />}
-       />
+      <Route path='github' element={<Github />} loader={githubInfoLoader} />
     </Route>
   )
 )
